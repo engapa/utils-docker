af63ea54ed1c4ad444805c8bcefbd84af3eec6aa
Enrique Garcia <engapa@gmail.com>
2017-01-25 11:17:57 +0100

    Debug information

--- common-functions.sh
+++ common-functions.sh
@@ -15,7 +15,7 @@ env_vars_in_file () {
   TO_SEPARATOR=${7:-${TO_SEPARATOR:-'.'}}     # Example: Convert FROM_SEPARATOR character/s into this, default to '.' .
   DEBUG=${8:-${DEBUG:-false}}                 # Activate debug mode
 
-  if $DEBUG =~true; then
+  if $DEBUG ; then
     echo -e "Writing environment variables to file :\n"
     echo "PREFIX           : ${PREFIX}"
     echo "DEST_FILE        : ${DEST_FILE}"
@@ -33,7 +33,7 @@ env_vars_in_file () {
     echo 'PREFIX and DEST_FILE are required values :-(' && exit 1;
   fi
 
-  if $CREATE_FILE =~ true && [ ! -f $DEST_FILE ]; then
+  if $CREATE_FILE && [ ! -f $DEST_FILE ]; then
     >>$DEST_FILE
   fi
 
@@ -42,6 +42,7 @@ env_vars_in_file () {
     ENV_VAR_NAME=`echo "${ENV_VAR}" | sed -r "s/=(.*)//"`
 
     if [ ! -z "${EXCLUSIONS}" ] && `echo "${ENV_VAR_NAME}" | egrep -q "${EXCLUSIONS}"`; then
+      $DEBUG =~ true && echo "[EXCLUDED] : ${ENV_VAR_NAME}"
       continue
     fi
 
@@ -50,11 +51,14 @@ env_vars_in_file () {
 
     if `egrep -q "(^|^#)${VAR_NAME}=.*" ${DEST_FILE}`; then
       # Not use '+' symbol neither in VAR_NAME nor in VAR_VALUE
-      $OVERRIDE =~ true && sed -r -i "s+(^|^#)${VAR_NAME}=.*+${VAR_NAME}=${VAR_VALUE}+g" ${DEST_FILE}
-      $OVERRIDE =~ false && sed -r -i "s+^#${VAR_NAME}=.*+${VAR_NAME}=${VAR_VALUE}+g" ${DEST_FILE}
+      $OVERRIDE && sed -r -i "s+(^|^#)${VAR_NAME}=.*+${VAR_NAME}=${VAR_VALUE}+g" ${DEST_FILE} \
+        && $DEBUG && echo "[OVERRIDE] : ${ENV_VAR_NAME} --> ${VAR_NAME}=${VAR_VALUE}"
+      [ ! $OVERRIDE ] && sed -r -i "s+^#${VAR_NAME}=.*+${VAR_NAME}=${VAR_VALUE}+g" ${DEST_FILE} \
+        && $DEBUG && echo "[ UPDATE ] : ${ENV_VAR_NAME} --> ${VAR_NAME}=${VAR_VALUE}"
     else
       # If VAR name not found in file, insert it at end of file
       echo "${VAR_NAME}=${VAR_VALUE}" >> ${DEST_FILE}
+      $DEBUG && echo "[  ADD   ] : ${ENV_VAR_NAME} --> ${VAR_NAME}=${VAR_VALUE}"
     fi
   done
 
